993b0c76c4d47a2cce381a9c4669bd22b17af0b1
Hqko01 <hakan.yilmaz0134@hotmail.com>
2026-09-22 16:03:58 +0300

    geolocation added

--- weather-app/app/page.tsx
+++ weather-app/app/page.tsx
@@ -1,69 +1,47 @@
-import Image from "next/image";
+'use client';
+
+import React from "react";
+import { useState, useEffect } from 'react';
+
 
 export default function Home() {
+  const API_KEY = process.env.NEXT_PUBLIC_OPENWEATHERAPIKEY;
+
+  const handleGetLocation = () => {
+    if (!navigator.geolocation) {
+      alert('Tarayıcınız konum özelliğini desteklemiyor.');
+      return;
+    }
+
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+        console.log(latitude, longitude);
+      },
+      (err) => {
+        console.log('Konum izni reddedildi veya konum alınamadı.');
+      }
+    );
+  };
+
+  useEffect(() => {
+    handleGetLocation();
+  }, []);
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert h-5 w-[100px]"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the{" "}
-            <code className="rounded bg-black/[.06] px-1.5 py-0.5 font-mono text-[0.9em] dark:bg-white/[.08]">
-              page.tsx
-            </code>{" "}
-            file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div className="card flex flex-col relative w-88 rounded-2xl text-white aspect-square bg-white/10 backdrop-blur-lg shadow-lg shadow-black/30 font-[montserrat]">
+      <div className="top flex-1 p-4 max-h-max">
+        <div className="loc">
+          <p className="text-lg font-bold">İstanbul, Beylikdüzü</p>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert h-[14px] w-4"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={14}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+        <div className="date text-[.9rem]">
+          <p>2023-10-10</p>
         </div>
-      </main>
+      </div>
+      <div className="center relative flex-2 border-2">
+        <div className="kelvin absolute top-2 left-2 text-5xl">25°C</div>
+      </div>
+      <div className="bottom flex-1 bg-amber-950"></div>
     </div>
   );
 }
